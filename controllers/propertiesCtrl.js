ccfe54b4ea3d544b5b0e8134b538d46f1151c293
Jerson Suaza <jerson.suaza@gmail.com>
2022-06-04 09:35:03 -0500

    properties layers

---
+++ controllers/propertiesCtrl.js
@@ -0,0 +1,26 @@
+const {
+  addProperty,
+  getProperties,
+  getProperty,
+} = require("./../services/propertiesService");
+
+const create = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const getAll = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const getDetail = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  create,
+  getAll,
+  getDetail,
+};
---
+++ services/propertiesService.js
@@ -0,0 +1,20 @@
+const addProperty = async () => {
+  try {
+  } catch (error) {}
+};
+
+const getProperties = async () => {
+  try {
+  } catch (error) {}
+};
+
+const getProperty = async () => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  addProperty,
+  getProperties,
+  getProperty,
+};
